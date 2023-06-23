c9861fce4d958bef353784dfa03c31fb7effd46a
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-06-23 04:51:15 -0700

    sortingalgorithm

---
+++ 103-merge_sort.c
@@ -0,0 +1,97 @@
+#include "sort.h"
+/**
+ * merge - Sort a subarray of integers.
+ * @array: A subarray of an array of integers to sort.
+ * @output: A buffer to store the sorted subarray.
+ * @low: The front index of the array.
+ * @mid: The middle index of the array.
+ * @high: The back index of the array.
+ */
+void merge(int *array, int *output, size_t low, size_t mid, size_t high)
+{
+	size_t i, j, k;
+
+	i = low;
+	j = mid + 1;
+	k = low;
+
+	printf("Merging...\n[left]: ");
+	print_array(array + low, mid - low + 1);
+	printf("[right]: ");
+	print_array(array + mid + 1, high - mid);
+	while (i <= mid && j <= high)
+	{
+		if (array[i] < array[j])
+		{
+			output[k] = array[i];
+			i++;
+		}
+		else
+		{
+			output[k] = array[j];
+			j++;
+		}
+		k++;
+	}
+	while (i <= mid)
+	{
+		output[k] = array[i], i++, k++;
+	}
+	while (j <= high)
+	{
+		output[k] = array[j], j++, k++;
+	}
+	for (i = low; i <= high; i++)
+	{
+		array[i] = output[i];
+	}
+	printf("[Done]: ");
+	print_array(array + low, high - low + 1);
+}
+/**
+ *mergesort - function that sorts an array of integers
+ *in ascending order using the Merge sort algorithm
+ *@array: array of integers
+ *@output: temp array used in merge, was created outside to
+ *optimize reducing the system calls
+ *@low: fisrt element position
+ *@high: last element position
+ *Return: void
+ */
+void mergesort(int *array, int *output, int low, int high)
+{
+	int mid;
+
+	mid = (low + high) / 2;
+
+	if (low < high)
+	{
+	mergesort(array, output, low, mid);
+	mergesort(array, output, mid + 1, high);
+	merge(array, output, low, mid, high);
+	}
+}
+
+/**
+ * merge_sort - Sort an array of integers in ascending
+ *              order using the merge sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Implements the top-down merge sort algorithm.
+ */
+void merge_sort(int *array, size_t size)
+{
+	int *newarray;
+
+	if (array == NULL || size < 2)
+		return;
+
+	newarray = malloc(sizeof(int) * size);
+	if (newarray == NULL)
+		return;
+
+	mergesort(array, newarray, 0, size - 1);
+
+	free(newarray);
+}
---
+++ 104-heap_sort.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+/**
+ * maxheapify - Turn a binary tree into a complete binary heap.
+ * @array: An array of integers representing a binary tree.
+ * @size: The size of the array/tree.
+ * @base: The index of the base row of the tree.
+ * @i: The max node of the binary tree.
+ */
+void maxheapify(int *array, int size, int base, int i)
+{
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+	int temp;
+
+	if (left < base && array[left] > array[largest])
+		largest = left;
+	if (right < base && array[right] > array[largest])
+		largest = right;
+	if (largest != i)
+	{
+		temp = array[largest];
+		array[largest] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+		maxheapify(array, size, base, largest);
+	}
+}
+/**
+ * heap_sort -  The main function to sort an array of given size
+ * @array: array to sort
+ * @size: size of the array
+ **/
+void heap_sort(int *array, size_t size)
+{
+	int temp, i;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = (size / 2) - 1; i >= 0; i--)
+		maxheapify(array, size, size, i);
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+		maxheapify(array, size, i, 0);
+	}
+}
